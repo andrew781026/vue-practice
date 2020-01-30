f0c9f245425e041205f46f114b3b934f27f9662e
王澍(Andrew) <andrewwang1@micb2b.com>
2020-01-30 16:40:21 +0800

    setting a todo list sample

--- hello-vue/src/components/HelloWorld.vue
+++ hello-vue/src/components/HelloWorld.vue
@@ -1,58 +1,90 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div :style="{width:'50vw',margin:'10px'}">
+
+        <b-input-group :style="{marginBottom:'20px'}">
+
+            <template v-slot:append v-if="Boolean(item)">
+                <b-input-group-text @click="clearItem">
+                    <strong class="text-danger">x</strong>
+                </b-input-group-text>
+            </template>
+
+            <b-form-input
+                    type="text"
+                    v-model="item"
+                    v-on:keyup.enter="addItem"
+                    placeholder="請輸入新的待辦事項"
+            />
+
+        </b-input-group>
+
+
+        <div class="todo" :key="index" v-for="(single,index) in list">
+
+            <b-form-checkbox v-model="single.done">
+
+                <del v-if="single.done === true">{{single.msg}}</del>
+                <span v-else>{{single.msg}}</span>
+
+            </b-form-checkbox>
+
+            <b-button variant="outline-secondary" :style="{marginLeft:'5px'}">編輯</b-button>
+            <b-button variant="outline-secondary" :style="{marginLeft:'5px'}" @click="del(single,index)">刪除</b-button>
+
+        </div>
+
+
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        methods: {
+            addItem: function () {
+
+                if (this.item) {
+
+                    this.list.push({msg: this.item});
+                    this.item = '';
+                }
+            },
+            clearItem: function () {
+                this.item = '';
+            },
+            del: function (item, index) {
+                this.list.splice(index, 1)
+            }
+        },
+        updated: function () {
+            // window.console.log('this.list=', this.list);
+        },
+        data() {
+            return {
+                item: this.item,
+                list: [
+                    {
+                        msg: '買一本好書',
+                        done: false,
+                    },
+                    {
+                        msg: '打電話給小明',
+                        done: true,
+                    },
+                    {
+                        msg: '寫一篇文章',
+                        done: false,
+                    },
+                ],
+            }
+        }
+    }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    .todo {
+        border: 1px solid rgba(0, 0, 0, 0.3);
+        display: flex;
+        padding: 10px;
+        align-items: center;
+    }
 </style>
